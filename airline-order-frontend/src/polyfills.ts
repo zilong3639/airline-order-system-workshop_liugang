9612e00cc7a6a101f26ae0f749088dcf784d4732
zhangkuo <769329489@qq.com>
2025-08-05 21:37:22 +0800

    移除zonejs

--- airline-order-frontend/src/polyfills.ts
+++
@@ -1,8 +0,0 @@
-// src/polyfills.ts
-
-/***************************************************************************************************
- * ZONE JS
- *
- * `Zone.js` is required by Angular for change detection.
- */
-// import 'zone.js';  // <== 这一行就是加载 Zone.js 的魔法！
